b6a9e0aaa391097e73da0c2efd18c1183588b353
dbwls <youjin9874@naver.com>
2021-09-08 14:47:29 +0900

    로그인DB,로그인 서비스기능

---
+++ src/A_yujin/LoginService.java
@@ -0,0 +1,7 @@
+package A_yujin;
+
+import javafx.scene.Parent;
+
+public interface LoginService {
+	public void loginChk(Parent root);
+}
--- src/main/Controller.java
+++ src/main/Controller.java
@@ -3,19 +3,30 @@ package main;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import A_yujin.LoginService;
+import main.DBClass;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 
 public class Controller implements Initializable{
 	Parent root;
+	public static DBClass db;
+	private LoginService ls;
 	public void setRoot(Parent root) {
 		this.root = root;
 		}
 
 		@Override
 		public void initialize(URL arg0, ResourceBundle arg1) {
+			db = new DBClass();
 		}
 		public void membership() {}
-		public void login() {}
-
+		
+		public void login() {
+			ls.loginChk(root);
+			
+			}
+			
 		}
+
+	
