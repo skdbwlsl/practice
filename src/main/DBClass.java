bf49009dc1151ad6c3a81ae4ee5a8bb9c9cbc473
dbwls <youjin9874@naver.com>
2021-09-08 12:55:36 +0900

    기본틀 작성, 팀플연습

---
+++ src/main/DBClass.java
@@ -0,0 +1,18 @@
+package main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBClass {
+	public Connection conn;
+	public DBClass() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn= DriverManager.getConnection("jdbc:oracle:thin:@210.221.253.215:1521:xe", "team222", "team222");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
